437b9564d9a36e2ac8736040adae05c876bc533b
Matheus Ribeiro <matheusgomes612@gmail.com>
2021-07-26 14:38:32 -0300

    Relacionamento um para um

---
+++ Alura.Loja.Testes.ConsoleApp/Endereco.cs
@@ -0,0 +1,14 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Endereco
+    {
+        public int Numero { get; internal set; }
+        public string Logradouro { get; internal set; }
+        public string Complemento { get; internal set; }
+        public string Bairro { get; internal set; }
+        public string Cidade { get; internal set; }
+
+        //Mapeando relacinamento de um para um com a tabela de endereços
+        public Cliente Cliente { get; set; }
+    }
+}
